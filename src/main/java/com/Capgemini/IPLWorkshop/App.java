ec0f32b435cf2858bb7f3c51175bb14e82cbb8fe
utkarshmega <utkarsheduc@gmail.com>
2020-10-21 09:27:27 +0530

    First Commit for IPL project

---
+++ src/main/java/com/Capgemini/IPLWorkshop/App.java
@@ -0,0 +1,13 @@
+package com.Capgemini.IPLWorkshop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
